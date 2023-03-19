a49a0f8e57f7e7a05767092171b8871843f918c8
keagud <keagud@protonmail.com>
2023-03-19 10:44:38 -0700

    tweak greeter script

--- greeter.sh
+++ greeter.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
 if  ! once.sh; then 
-  exit 0
+  echo "a" > /dev/null
 fi
 
 UPDATES="$(checkupdates)"
-UPDATEMSG="\t$(echo "$UPDATES" | wc -l) packages can be updated"
+UPDATEN=$(echo "$UPDATES" | grep -E -cvo '^\s*$')
+
+UPDATEMSG=""
+
+if [[ $UPDATEN > 0 ]]; then
+  UPDATEMSG="\t\n$UPDATEN packages can be updated"
+fi
+
 KERNELUPDATE=$(echo "$UPDATES" | grep -c -o "^linux\s")
 
 KERNELMSG=""
-if [[ KERNELUPDATE ]]; then
+if [[ $KERNELUPDATE > 0 ]]; then
   KERNELMSG="\n\tKernel update available!"
 fi
 
-echo -e  "\n\033[1m\t$(date +"%A, %B %e %Y\t%-j/365" )\033[0m\n\n$UPDATEMSG$KERNELMSG" | lolcat -a
+echo -e  "\n\033[1m\t$(date +"%A, %B %e %Y\t%-j/365" )\033[0m\n$UPDATEMSG$KERNELMSG" | lolcat -a
 
