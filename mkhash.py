8d9d8772eddc3a9178af6af6c1673c4dc8a8f673
keagud <keagud@protonmail.com>
2023-09-22 15:01:49 -0700

    update mkhash.py to use sha-3

--- mkhash.py
+++ mkhash.py
@@ -1,6 +1,7 @@
 #!/bin/env python3
 import hashlib
 from secrets import token_hex
+from secrets import token_bytes
 import argparse
 
 import time
@@ -22,17 +23,17 @@ def main():
     )
     args = parser.parse_args()
 
-    secret: str = token_hex(512)
-    key: str = hashlib.sha512(secret.encode()).hexdigest()
+    secret_bytes: bytes = token_bytes(512)
+    key_bytes: bytes = hashlib.sha3_512(secret_bytes).digest()
 
     key_path: Path = args.key_path
     secret_path: Path = args.secret_path
 
     with open(key_path, "w") as fp:
-        fp.write(key.strip())
+        fp.write(key_bytes.hex())
 
     with open(secret_path, "w") as fp:
-        fp.write(secret.strip())
+        fp.write(secret_bytes.hex())
 
 
 if __name__ == "__main__":
