81b4609d67288219184eaaad354aae83e4058611
keagud <keagud@alloquor.net>
2024-04-13 10:45:19 -0700

    update docker install script

--- installs/docker.sh
+++ installs/docker.sh
@@ -15,7 +15,7 @@ sudo chmod a+r /etc/apt/keyrings/docker.asc
 # Add the repository to Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  bookworm stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
@@ -28,3 +28,4 @@ sudo systemctl enable docker.service
 sudo systemctl enable containerd.service
 
 newgrp docker
+sudo usermod -aG docker $USER
